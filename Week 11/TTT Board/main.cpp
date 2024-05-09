cae140a0b638e50447e36187f1bc696f589a8341
Owen Ceresa <142851013+CO84635@users.noreply.github.com>
2024-05-09 15:01:39 -0500

    Portfolio 3 Stuff I guess

--- Week 11/TTT Board/main.cpp
+++ Week 11/TTT Board/main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
-//#include "TTT_Logic.h"
 
-void game_board(char number[9]) {
+void game_board(std::string number[9]) {
     std::cout << "    " << number[0] << "  |  " << number[1] << "  |  " << number[2] << "\n" <<
               "  -----+-----+-----\n" <<
               "    " << number[3] << "  |  " << number[4] << "  |  " << number[5] << "\n" <<
@@ -10,46 +9,69 @@ void game_board(char number[9]) {
 }
 
 int main() {
-    std::string mark_choice;
-    std::cout << "Would you like to choose a mark?" << std::endl;
-    getline(std::cin, mark_choice);
-    if (mark_choice == "Yes" ) {
-        std::cout << "What is the player one mark?" << std::endl;
-    } else {
-        std::cout << "No alternate player mark selected" << std::endl;
-    }
-
-
-    char number[9] = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
-
-    game_board(number);
+    std::string number[9] = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
 
     int i = 9;
     int z = 0;
+    std::string player_one_mark = "X";
+    std::string player_two_mark = "O";
+
     while (i > 0) {
-        char mark;
+        std::string mark_choice;
+        std::cout << "Would you like to choose a mark?" << std::endl;
+        getline(std::cin, mark_choice);
+
+        if (mark_choice == "Yes" || mark_choice == "yes" || mark_choice == "Y" || mark_choice == "y") {
+            bool player_one_mark_length_format = false;
+            while(!player_one_mark_length_format) {
+                std::cout << "What is player one's mark?" << std::endl;
+                std::cin >> player_one_mark;
+                if (player_one_mark.size() == 1) {
+                    player_one_mark_length_format = true;
+                } else {
+                    std::cout << "Incorrect Length!" << std::endl;
+                }
+            }
+
+            bool player_two_mark_length_format = false;
+            while(!player_two_mark_length_format) {
+                std::cout << "What is player two's mark?" << std::endl;
+                std::cin >> player_two_mark;
+                if (player_two_mark.size() == 1) {
+                    player_two_mark_length_format = true;
+                } else {
+                    std::cout << "Incorrect Length!" << std::endl;
+                }
+            }
+
+        } else if (mark_choice == "exit" || mark_choice == "Exit" || mark_choice == "E" || mark_choice == "e") {
+            break;
+        } else {
+            std::cout << "No alternate player marks selected\n" << std::endl;
+        }
+
         std::string input;
-        std::cout << "What is your move? " << std::endl;
+        game_board(number);
+        std::cout << "\nWhat is your move? " << std::endl;
         std::cin >> input;
-        if (input == "exit") {
-            std::cout << "Goodbye!" << std::endl;
+        if (input == "exit" || input == "Exit" || input == "E" || input == "e") {
             break;
         }
         int move = std::stoi(input);
-        if (move >= 1 && move <= 9 && number[move - 1] != 'X' && number[move - 1] != 'O'){
+        if (move >= 1 && move <= 9 && number[move - 1] != player_one_mark && number[move - 1] != player_two_mark){
             if (z % 2 == 0) {
-                mark = 'X';
+                number[move - 1] = player_one_mark;
             } else {
-                mark = 'O';
+                number[move - 1] = player_two_mark;
             }
-            number[move - 1] = mark;
-            game_board(number);
             --i;
             ++z;
         } else {
-            std::cout << "This spot is already taken!" << std::endl;
+            std::cout << "This spot is invalid!" << std::endl;
         }
+        game_board(number);
     }
+    std::cout << "\nThank you for playing my Tic-Tac-Toe program!";
     return 0;
 }
 
